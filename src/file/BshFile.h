bf6dcac8533b759040a85c5894ee7f4502bca0a0
stwe <no@mail.tt>
2022-07-16 23:07:16 +0200

    add copyr. header

--- src/file/BshFile.h
+++ src/file/BshFile.h
@@ -1,3 +1,21 @@
+// This file is part of the MDCII project.
+//
+// Copyright (c) 2022. stwe <https://github.com/stwe/MDCII>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 #pragma once
 
 #include "BinaryFile.h"
@@ -94,12 +112,12 @@ namespace mdcii::file
         //-------------------------------------------------
 
         /**
-         * If the next byte is 0xFF the image has reached it’s end.
+         * If the next byte is 0xFF the image has reached itï¿½s end.
          */
         static constexpr uint8_t END_MARKER{ 255 };
 
         /**
-         * If the next byte is 0xFE the current pixel line has reached it’s end.
+         * If the next byte is 0xFE the current pixel line has reached itï¿½s end.
          */
         static constexpr uint8_t END_OF_ROW{ 254 };
 
