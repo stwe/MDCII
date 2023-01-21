ddef7b579850bad8f83f454929da9a45ab813011
stwe <stwe@no_mail.tt>
2023-01-21 22:40:41 +0100

    fix missing include

--- src/MdciiUtils.cpp
+++ src/MdciiUtils.cpp
@@ -17,6 +17,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 #include <fstream>
+#include <filesystem>
 #include <imgui.h>
 #include "MdciiUtils.h"
 #include "Game.h"
