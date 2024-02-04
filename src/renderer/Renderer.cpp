5ea92d3ad216f193f24887a3888d7e3a844cf87b
stwe <no@mail.tt>
2024-02-04 20:38:24 +0100

    Moved some methods to the renderer.

--- src/renderer/Renderer.cpp
+++ src/renderer/Renderer.cpp
@@ -19,8 +19,13 @@
 #include "Renderer.h"
 #include "Game.h"
 #include "world/World.h"
-#include "world/tile/TerrainTile.h"
+#include "world/Island.h"
+#include "world/DeepWater.h"
+#include "world/layer/TerrainLayer.h"
+#include "world/layer/FiguresLayer.h"
 #include "resource/AssetManager.h"
+#include "resource/TileAtlas.h"
+#include "resource/AnimalsTileAtlas.h"
 #include "camera/Camera.h"
 #include "state/State.h"
 
@@ -50,4 +55,135 @@ void mdcii::renderer::Renderer::RenderAsset(
         { 1.0f, 1.0f },
         t_tint
     );
+}
+
+void mdcii::renderer::Renderer::RenderIsland(const world::World* t_world, world::Island* t_island, const world::layer::LayerType t_layerType)
+{
+    for (auto& terrainTile : t_island->GetTerrainLayer(t_layerType)->currentTiles)
+    {
+        if (terrainTile.HasBuilding())
+        {
+            terrainTile.UpdateFrame(t_world->tileAtlas->frame_values);
+            t_world->tileAtlas->RenderTile(t_island->startX, t_island->startY, &terrainTile, olc::WHITE);
+            if (t_world->renderIslandsGrid && t_layerType != world::layer::LayerType::COAST)
+            {
+                RenderAsset(resource::Asset::GREEN_ISO, t_island->startX, t_island->startY, t_world, &terrainTile, true);
+            }
+        }
+
+        if (t_world->HasRenderLayerOption(world::RenderLayer::RENDER_FIGURES_LAYER))
+        {
+            const auto rotationInt{ magic_enum::enum_integer(t_world->camera->rotation) };
+            const auto idx{ t_island->GetFiguresLayer()->sortedIndices.at(rotationInt).at(world::tile::Tile::GetRenderIndex(
+                terrainTile.posX,
+                terrainTile.posY,
+                t_island->GetFiguresLayer()->width,
+                t_island->GetFiguresLayer()->height,
+                t_world->camera->rotation))
+            };
+            auto& figureTile{ t_island->GetFiguresLayer()->sortedTiles.at(rotationInt).at(idx) };
+            if (figureTile.HasFigure())
+            {
+                figureTile.UpdateFrame(t_world->animalsTileAtlas->frame_values);
+                t_world->animalsTileAtlas->RenderTile(t_island->startX, t_island->startY, &figureTile, olc::WHITE);
+            }
+        }
+    }
+}
+
+void mdcii::renderer::Renderer::RenderWorldParts(const world::World* t_world, const float t_elapsedTime)
+{
+    // update animations
+    t_world->tileAtlas->CalcAnimationFrame(t_elapsedTime);
+    t_world->animalsTileAtlas->CalcAnimationFrame(t_elapsedTime);
+
+    // render deep water
+    if (t_world->HasRenderLayerOption(world::RenderLayer::RENDER_DEEP_WATER_LAYER))
+    {
+        for (auto &waterTile: t_world->deepWater->layer->currentTiles)
+        {
+            waterTile.UpdateFrame(t_world->tileAtlas->frame_values);
+            t_world->tileAtlas->RenderTile(0, 0, &waterTile, olc::WHITE);
+            if (t_world->renderDeepWaterGrid)
+            {
+                RenderAsset(resource::Asset::BLUE_ISO, 0, 0, t_world, &waterTile, false);
+            }
+        }
+    }
+
+    // render islands
+    for (auto const& island : t_world->currentIslands)
+    {
+        if (t_world->HasRenderLayerOption(world::RenderLayer::RENDER_MIXED_LAYER))
+        {
+            RenderIsland(t_world, island, world::layer::LayerType::MIXED);
+        }
+        else
+        {
+            if (t_world->HasRenderLayerOption(world::RenderLayer::RENDER_COAST_LAYER))
+            {
+                RenderIsland(t_world, island, world::layer::LayerType::COAST);
+            }
+            if (t_world->HasRenderLayerOption(world::RenderLayer::RENDER_TERRAIN_LAYER))
+            {
+                RenderIsland(t_world, island, world::layer::LayerType::TERRAIN);
+            }
+            if (t_world->HasRenderLayerOption(world::RenderLayer::RENDER_BUILDINGS_LAYER))
+            {
+                RenderIsland(t_world, island, world::layer::LayerType::BUILDINGS);
+            }
+        }
+    }
+}
+
+void mdcii::renderer::Renderer::RenderWorld(const world::World* t_world, const float t_elapsedTime)
+{
+    // update animations
+    t_world->tileAtlas->CalcAnimationFrame(t_elapsedTime);
+    t_world->animalsTileAtlas->CalcAnimationFrame(t_elapsedTime);
+
+    // render deep water
+    for (auto& waterTile : t_world->deepWater->layer->currentTiles)
+    {
+        waterTile.UpdateFrame(t_world->tileAtlas->frame_values);
+        t_world->tileAtlas->RenderTile(0, 0, &waterTile, olc::WHITE);
+        if (t_world->renderDeepWaterGrid)
+        {
+            RenderAsset(resource::Asset::BLUE_ISO, 0, 0, t_world, &waterTile, false);
+        }
+    }
+
+    // render islands
+    for (auto const& island : t_world->currentIslands)
+    {
+        for (auto& terrainTile : island->GetTerrainLayer(world::layer::LayerType::MIXED)->currentTiles)
+        {
+            // render mixed terrain tile
+            if (terrainTile.HasBuilding())
+            {
+                terrainTile.UpdateFrame(t_world->tileAtlas->frame_values);
+                t_world->tileAtlas->RenderTile(island->startX, island->startY, &terrainTile, olc::WHITE);
+                if (t_world->renderIslandsGrid)
+                {
+                    RenderAsset(resource::Asset::GREEN_ISO, island->startX, island->startY, t_world, &terrainTile, true);
+                }
+            }
+
+            // render a given figure on top of the mixed terrain tile
+            const auto rotationInt{ magic_enum::enum_integer(t_world->camera->rotation) };
+            const auto idx{ island->GetFiguresLayer()->sortedIndices.at(rotationInt).at(world::tile::Tile::GetRenderIndex(
+                terrainTile.posX,
+                terrainTile.posY,
+                island->GetFiguresLayer()->width,
+                island->GetFiguresLayer()->height,
+                t_world->camera->rotation))
+            };
+            auto& figureTile{ island->GetFiguresLayer()->sortedTiles.at(rotationInt).at(idx) };
+            if (figureTile.HasFigure())
+            {
+                figureTile.UpdateFrame(t_world->animalsTileAtlas->frame_values);
+                t_world->animalsTileAtlas->RenderTile(island->startX, island->startY, &figureTile, olc::WHITE);
+            }
+        }
+    }
 }
--- src/renderer/Renderer.h
+++ src/renderer/Renderer.h
@@ -30,6 +30,11 @@ namespace mdcii::world
      * @brief Forward declaration class World.
      */
     class World;
+
+    /**
+     * @brief Forward declaration class Island.
+     */
+    class Island;
 }
 
 namespace mdcii::world::tile
@@ -38,6 +43,14 @@ namespace mdcii::world::tile
      * @brief Forward declaration struct TerrainTile.
      */
     struct TerrainTile;
+}
+
+namespace mdcii::world::layer
+{
+    /**
+     * @brief Forward declaration enum class LayerType.
+     */
+    enum class LayerType;
 }
 
 namespace mdcii::resource
@@ -54,8 +67,6 @@ namespace mdcii::renderer
     // Renderer
     //-------------------------------------------------
 
-    // todo: -> AssetManager
-
     /**
      * @brief Represents a Renderer.
      */
@@ -66,6 +77,17 @@ namespace mdcii::renderer
         // Logic
         //-------------------------------------------------
 
+        /**
+         * @brief Renders a specific asset on the screen.
+         *
+         * @param t_asset The asset to render.
+         * @param t_startX The x offset.
+         * @param t_startY The y offset.
+         * @param t_world Pointer to the World object.
+         * @param t_tile Pointer to the TerrainTile object associated with the asset.
+         * @param t_minusElevation Indicates whether to adjust for elevation.
+         * @param t_tint Color tint for the asset (default is white).
+         */
         static void RenderAsset(
             resource::Asset t_asset,
             int t_startX,
@@ -75,6 +97,31 @@ namespace mdcii::renderer
             bool t_minusElevation,
             const olc::Pixel& t_tint = olc::WHITE
         );
+
+        /**
+         * @brief Renders an island based on the specified layer type.
+         *
+         * @param t_world Pointer to the World object.
+         * @param t_island Pointer to the Island object to be rendered.
+         * @param t_layerType The type of layer to render.
+         */
+        static void RenderIsland(const world::World* t_world, world::Island* t_island, world::layer::LayerType t_layerType);
+
+        /**
+         * @brief Renders various parts of the world, such as deep water and islands.
+         *
+         * @param t_world Pointer to the World object.
+         * @param t_elapsedTime The elapsed time for animations.
+         */
+        static void RenderWorldParts(const world::World* t_world, float t_elapsedTime);
+
+        /**
+         * @brief Renders the entire world, including deep water, islands and figures.
+         *
+         * @param t_world Pointer to the World object.
+         * @param t_elapsedTime The elapsed time for animations.
+         */
+        static void RenderWorld(const world::World* t_world, float t_elapsedTime);
 
     protected:
 
